a79a2ddb21dd7cf0257b76b994274cae45fa10f0
Fr3574 <frederik.stalschus@live.de>
2023-02-05 21:49:23 +0100

    fix naming

--- scripts/runVerticalBenchmark.sh
+++ scripts/runVerticalBenchmark.sh
@@ -8,18 +8,18 @@ sudo docker run --rm \
     -d \
     -v $PWD/benchmark_output:/app/benchmark_output \
     --name benchmark_1 \
-    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -name=benchmark_1
+    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -container_name=benchmark_1
 
 sudo docker run --rm \
     --net bridge \
     -d \
     -v $PWD/benchmark_output:/app/benchmark_output \
     --name benchmark_2 \
-    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -name=benchmark_2
+    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -container_name=benchmark_2
 
 sudo docker run --rm \
     --net bridge \
     -d \
     -v $PWD/benchmark_output:/app/benchmark_output \
     --name benchmark_3 \
-    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -name=benchmark_3
+    benchmark:latest -sut=${sut} -mode=vertical -trace_length=${traceLength} -increment_interval=${incrementInterval} -container_name=benchmark_3
